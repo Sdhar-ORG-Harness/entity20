2abd6475672db5474437ddd8d86810c5f4b0b3ba
ShibamD <>
2025-07-18 10:52:27 +0000

    Project init

---
+++ Impact1705 try asdertyui/src/main/java/org/Impact1705 try asdertyui/Application.java
@@ -0,0 +1,13 @@
+package org.Impact1705 try asdertyui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ Impact1705 try asdertyui/src/test/java/org/Impact1705 try asdertyui/controller/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package org.Impact1705 try asdertyui.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        HomeController homeController = new HomeController();
+        assertEquals("Hello world", homeController.home());
+    }
+}
